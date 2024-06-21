bbfb0ee40924fbc5d294e1713780617774d1664d
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2024-06-21 12:33:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -190,16 +190,16 @@ cards.forEach(box => {
             let timer =0;
                       if(cardLeft.innerText == 0)
                         {
-                          
-                
+                          setTimeout(()=>{
                               body.style.backgroundColor ="#023047"
-                                cards.forEach(card=>{
+                              cards.forEach(card=>{
                                 card.style.backgroundColor ="#023047"
                                 card.firstChild.style.color ="#023047"
                                 card.style.border ="none"
                                 card.firstChild.style.visibility  ="hidden" 
-                                card.style.visibility = 'visible'
-                                })
+                                card.style.visibility  ="hidden" 
+                              })
+                             
                               setTimeout(()=>{
                                 popUp.style.visibility = 'visible'
                                 popUp.style.top = '50%'
